8d2f9ba0b61f684aa9f47a78c702e1f1f3f6b222
Nic1493 <nic.choy107@gmail.com>
2020-12-30 01:26:59 -0500

    Folder hierarchy re-structure

---
+++ Assets/Scripts/Game/HUD/HintCounter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HintCounter : MonoBehaviour
+{
+    TextMeshProUGUI hintsRemainingText;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
